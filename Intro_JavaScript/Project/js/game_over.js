2bc4083eb6056a081af53fe9e8bb4dd03ca088e4
Luke Kucinski <lukek22b@gmail.com>
2024-12-19 12:29:44 +0100

    game over file

---
+++ Intro_JavaScript/Project/js/game_over.js
@@ -0,0 +1,7 @@
+/* Time over function
+Time over function to add the time over message
+  Select the timeOver element and add the visible class to display the message
+*/
+function timeOver() {
+  document.querySelector("#timeOver").classList.add("visible");
+}
