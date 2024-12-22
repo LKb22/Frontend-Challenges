a74661830db0eb3c21fdce181694c08d38edd12f
Luke Kucinski <lukek22b@gmail.com>
2024-12-22 15:53:15 +0100

    updated game initialization

--- Intro_JavaScript/Project/js/start.js
+++ Intro_JavaScript/Project/js/start.js
@@ -1,4 +1,4 @@
-/* Create a guessing
+/* Create a guessing game
 Create a guessing game using 'cards'
     Create a deck of cards using emojis
     Deal the cards to the table div display
@@ -10,43 +10,53 @@ Create a guessing game using 'cards'
 /* Initialize the game
 Create the start function to initialize the game
   Set the move counter to 0
-  Set the move display to 00
+    Set the move display to 00
   Call the dealCards() function to deal the cards to the table
-  Call the maxCounter() function to set the maximum number of moves for the current level
-  Hide the menu and modals
+    Pass the number of cards to deal to the dealCards() function from the levels object using the levelActual variable based on the current level
+  Call the maxCounter() function to check and set the maximum number of moves for the current level
+  Hide the welcome page, menu, and modals
+    Hide the welcome page
     Hide select level menu
     Hide the level up modal
     Hide the game over modal
     Hide the time over modal
     Hide the end game modal
+  Show the table
   Add click event listener to each card
-  Start the timer if the game is not in relax mode
+    Pass the discover function to the click event listener to give the cards their functionality
+  Start and unhide the timer if the game is not in relax mode
     Else, hide the timer
+  Toggle the header visibility for the weclome page and cards table
 */
 // Create the start function to initialize the game
 function start() {
   // Set the move counter to 0
   moves = 0;
   // Set the move display to 00
-  document.querySelector("#moves").innerText = "00";
+  document.querySelector("#mov").innerText = "00";
   // Call the dealCards() function
-  dealCards();
+  dealCards(levels[levelActual].cards);
   // Call the maxCounter() function
   maxCounter();
-  // Hide the menu and modals
-// Hide the menu and modals
-[".select-level", "#levelUp", "#gameOver", "#timeOver", "#endGame"].forEach(selector => {
-  document.querySelector(selector).classList.remove("visible");
-});
-    // Add click event listener to each card
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', flip);
+  // Hide the welcome page, menu, and modals
+  ["#welcome", ".select-level", "#endGame", "#timeOver", "#gameOver", "#nextLevel"].forEach(selector => {
+    document.querySelector(selector).classList.remove("visible");
   });
-  // Start the timer if the game is not in relax mode
-  if (!relaxMode) {
-    startTimer();
+  // Show the table
+  document.querySelector("#table").classList.remove("hidden");
+  // Add click event listener to each card
+  document.querySelectorAll(".card").forEach(function(element) {
+    element.addEventListener("click", discover);
+  });
+  // If the game is not in relax mode
+  if (!modeRelax) {
+    // Start the timer
+    initiateStopwatch();
+    // Unhide the timer
+    document.querySelector("#stopwatch").classList.remove("stopwatch-hidden");
+    // Else, hide the timer
   } else {
-    document.querySelector("#stopwatch").classList.add(".hidden-control");
+    document.querySelector("#stopwatch").classList.add("stopwatch-hidden");
   }
 }
 
@@ -59,9 +69,9 @@ Create the restart function to reset the game
 // Create the restart function to reset the game
 function restart() {
   // Reset the current level to the first level
-  currentLevel = 0;
+  levelActual = 0;
   // Update the level display
-  updatelevel();
+  updateLevel();
   // Start the game again
   start();
 }
@@ -69,34 +79,51 @@ function restart() {
 /* Normal Mode
 Create the normal game mode function
   Set to normal game mode (relax mode false)
-  Hide the welcome modal
-  Start the game
   Hide the level control modal
+  Start the game
 */
 // Create the normal game mode function
-function startNormalMode() {
+function initiateModeNormal() {
   // Set to normal mode
-  relaxMode = false;
-  // Hide the welcome modal
-  document.querySelector("#welcome").classList.remove("visible");
+  modeRelax = false;
+  // Hide the level control modal
+  document.querySelector(".control-level").classList.add("control-hidden");
   // Start the game
   start();
-  // Hide the level control modal
-  document.querySelector(".select-level").classList.add(".hidden-control");
 }
 
 /* Relax Mode
 Create the relax game mode function
   Set to relax game mode (relax mode true)
-  Hide the welcome modal
+  Add the level control modal
   Start the game
 */
 // Create the relax game mode function
-function startRelaxMode() {
+function initiateModeRelax() {
   // Set to relax mode
-  relaxMode = true;
-  // Hide the welcome modal
-  document.querySelector("#welcome").classList.remove("visible");
+  modeRelax = true;
+  // Add the level control modal
+  document.querySelector(".control-level").classList.remove("control-hidden");
   // Start the game
   start();
+}
+
+/* Toggle header visibility
+Create the toggleHeaderVisibility function to toggle the header visibility
+  Get the header and welcome page elements
+  If the welcome page is visible, hide the header
+  Else, show the header
+*/
+// toggleHeaderVisibility function
+function toggleHeaderVisibility() {
+  // Get the header and welcome page elements
+  const header = document.querySelector('.header');
+  const welcomePage = document.querySelector('.welcome');
+  // If the welcome page is visible, hide the header
+  if (welcomePage.classList.contains('visible')) {
+    header.classList.add('header-hidden');
+  // Else, show the header
+  } else {
+    header.classList.remove('header-hidden');
+  }
 }
