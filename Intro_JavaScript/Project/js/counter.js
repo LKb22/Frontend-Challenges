03e9a442cfb4afcfb1644725a7458e9ddee676ed
Luke Kucinski <lukek22b@gmail.com>
2024-12-22 11:00:08 +0100

    counter and deal cards updated

--- Intro_JavaScript/Project/js/counter.js
+++ Intro_JavaScript/Project/js/counter.js
@@ -20,12 +20,22 @@ Create a function to track and update the number of moves
   Update the display variable with the number of moves
 */
 function updateCounter() {
+  // Initialize the number of moves variable
   let movesText;
+  // Increment the number of moves by one each time the function runs
   moves++;
+  // Update the display variable with the number of moves
   movesText = moves;
-
+  // Log the information to the console
+  console.log("Current number of moves", moves);
+  console.log("Relax mode activated?:", modeRelax);
+  console.log("Current level:", levels[levelActual]);
+  console.log("Max moves allowed:", levels[levelActual].movesMax);
+  // Limit the number of moves
   if (moves > levels[levelActual].movesMax && !modeRelax) {
+    // Call the gameOver function if the number of moves exceeds the limit
     gameOver();
+    // Exit the function
     return;
   }
   /* Use double digits
@@ -36,9 +46,11 @@ function updateCounter() {
     Update the moves in the display using innerText
   */
   // Create the if statements
+  // If the moves are less than 10, add a leading zero
   if (moves < 10) {
     movesText = "0" + moves;
   }
+  // Update the moves in the display using innerText
   document.querySelector("#mov").innerText = movesText;
 }
 /* Create the maxCounter function
@@ -48,10 +60,14 @@ Create the maxCounter function to set the maximum number of moves for each level
     Use double digits
     Update the display
 */
+// Create the maxCounter function
 function maxCounter() {
+  // Retrieve the maximum number of moves for the current level
   let movesMaxText = levels[levelActual].movesMax;
+  // Use double digits if the number of moves is less than 10
   if (movesMaxText < 10) {
     movesMaxText = "0" + movesMaxText;
   }
+  // Update the display using innerText
   document.querySelector("#mov-total").innerText = movesMaxText;
 }
--- Intro_JavaScript/Project/js/deal-cards.js
+++ Intro_JavaScript/Project/js/deal-cards.js
@@ -1,33 +1,53 @@
-/* Deal the cards
-Deal the cards
-  Create a function to deal the cards
-  The cards will be dealt into the table div
-  Clear the table div before adding the cards
-  Each emoji in the finalShuffledEmojis array will be added to a card div
-  Iterate over the finalShuffledEmojis array using forEach()
-  For each emoji in the array, create a card div element
-  Use two divs for each card for styling, an inner and outer div
-  Add the emoji to the inner card div
-  Use template literals for multi-line string interpolation
-  Add the data-value attribute to the card div to store the emoji value, so we can check for matches later on
-  Append the card div to the table div
-*/
-// Function to deal the cards to the table
-function dealCards() {
-  const table = document.getElementById('table');
-  table.innerHTML = ''; // Clear existing cards
+function deckCards(theCards) {
+  let result;
+  const totalCards = theCards.concat(theCards);
+  result = totalCards.sort(function() {
+    return 0.5 - Math.random();
+  });
+  return result;
+}
 
-  const emojis = createCardGroup();
-  emojis.forEach((emoji) => {
-    const card = document.createElement('div');
-    card.innerHTML = `
-      <div class='card' data-value='${emoji}'>
-        <div class='card_content'>
-          ${emoji}
-        </div>
-      </div>
-    `;
+/* Function to deal the cards
+Create a function to deal the cards
+  Select the table element
+  Call the deckCards function to get an array of shuffled cards
+  Clear the table
+  Loops through the cards
+    Create a div element and add the class card for styling
+      Add the data-valor attribute
+    Create a child div element for the card content and add the class card__content for styling
+    Add the text content(element) to the card content as plain text
+    Append the card content to the card
+    Append the card to the table
+    Log the cards added to the console
+*/
+// Function to deal the cards
+function dealCards(theCards) {
+  // Access the tabel element
+  const table = document.querySelector("#table");
+  // Get an array of shuffled cards
+  let cardsShuffled = deckCards(theCards);
+  // Clear the table
+  table.innerHTML = "";
+  // Loop through the shuffled cards
+  cardsShuffled.forEach(function(element) {
+    // Create a div element
+    let card = document.createElement("div");
+    // Add the class card for styling
+    card.classList.add("card");
+    // Add the data-valor attribute
+    card.setAttribute("data-valor", element);
+    // Create a child div element for the card content
+    let cardContent = document.createElement("div");
+    // Add the class card__content
+    cardContent.classList.add("card__content");
+    // Add the text content(element) to the card content as plain text
+    cardContent.textContent = element;
+    // Append the card content to the card
+    card.appendChild(cardContent);
+    // Append the card to the table
     table.appendChild(card);
+    // Log the cards added to the console
     console.log("Cards added", card.index);
   });
 }
