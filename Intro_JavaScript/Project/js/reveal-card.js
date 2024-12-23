106dd8c53dbca3317ce33052fff33b6a82f60a88
Luke Kucinski <lukek22b@gmail.com>
2024-12-23 14:59:10 +0100

    reveal, compare cards files

--- Intro_JavaScript/Project/js/reveal-card.js
+++ Intro_JavaScript/Project/js/reveal-card.js
@@ -57,23 +57,3 @@ const discover = function () {
     setTimeout(finish, 1000);
   };
 };
-
-/* Function to compare two flipped cards
-Create a function to compare the two flipped cards.
-  The function should take an array of two flipped cards as an argument (opened)
-  If the two flipped cards are the same, pass them to the success function.
-  Else, pass them to the error function.
-*/
-// Compare function
-const compare = (cardsCompare) => {
-  // Get the cards by their index and compare them using the "value" attribute
-  if (
-    cardsCompare[0].dataset.value === cardsCompare[1].dataset.value
-  ) {
-    // Same - success
-    success(cardsCompare);
-    // Else - error
-  } else {
-    error(cardsCompare);
-  };
-};
