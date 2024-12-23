75cfaf6cf19a9f80b17d72b45f362b3a385dc982
Luke Kucinski <lukek22b@gmail.com>
2024-12-23 15:12:24 +0100

    level finished, game restart

---
+++ Intro_JavaScript/Project/js/game-restart.js
@@ -0,0 +1,15 @@
+/* Restart function
+Create the restart function to reset the game
+  Reset the current level to the first level
+  Update the level display
+  Start the game again
+*/
+// Create the restart function to reset the game
+const restart = () => {
+  // Reset the current level to the first level
+  levelActual = 0;
+  // Update the level display
+  updateLevel();
+  // Start the game again
+  start();
+};
--- Intro_JavaScript/Project/js/level-finished.js
+++ Intro_JavaScript/Project/js/level-finished.js
@@ -1,5 +1,5 @@
-/* End game the game
-Create a function to end the game
+/* Level finished function
+Create a function to handle the end of each level
   Clear the stopwatch interval
   If the current level is not the last level, show the "Next Level" button
   Else, show the "End Game" button
@@ -16,20 +16,4 @@ const finish = () => {
   } else {
     document.querySelector("#endGame").classList.add("visible");
   };
-};
-
-/* Restart function
-Create the restart function to reset the game
-  Reset the current level to the first level
-  Update the level display
-  Start the game again
-*/
-// Create the restart function to reset the game
-const restart = () => {
-  // Reset the current level to the first level
-  levelActual = 0;
-  // Update the level display
-  updateLevel();
-  // Start the game again
-  start();
 };
